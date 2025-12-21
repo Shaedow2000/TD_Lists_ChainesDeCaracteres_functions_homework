21bb274cdb464dc46c3d1976de17970f0826a454
Shaedow2000 <amsal8795@gmail.com>
2025-12-21 17:54:43 +0100

    Do ex 10.

---
+++ ex_10.py
@@ -0,0 +1,28 @@
+import sys
+
+def msg_voyelles( msg: str ) -> str:
+    voyelles: list[ str ] = [ 'a', 'e', 'i', 'o', 'u', 'y' ]
+    
+    voyelles_trouver: list[ str ] = []
+
+    for i in range( len( msg ) ):
+        if msg[ i ] in voyelles:
+            voyelles_trouver.append( msg[ i ] )
+
+    return ' '.join( voyelles_trouver )
+
+
+def main() -> None:
+    while True:
+        msg: str = input( '=> Message: ' )
+
+        print( f'--> Les voyelles trouver sont: { msg_voyelles( msg ) }' )
+
+        break
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print( '\n--> Exiting...' )
+        sys.exit( 1 )
