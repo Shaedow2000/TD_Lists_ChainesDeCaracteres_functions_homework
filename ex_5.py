308c4686e2e9664e6bc6a82566827a9637823845
Shaedow2000 <amsal8795@gmail.com>
2025-12-21 14:48:54 +0100

    Do ex 5.

--- ex_5.py
+++ ex_5.py
@@ -4,20 +4,23 @@ def palindrome( li: list ) -> bool:
     lenght: int = len( li )
     middle: int = floor( lenght / 2 )
 
-    print( middle )
+    pal: bool = False
 
     for i in range( lenght ):
         if i <= ( middle - 1 ):
             j: int = -i-1
             if li[ i ] == li[ j ]:
-                pass
+                pal = True 
     
-    return True
+    return True if pal else False
 
 def main() -> None:
     list_chars: list[ str ] = list( input( '==> ' ) )
 
-    palindrome(list_chars)
+    if palindrome(list_chars):
+        print( '-> This is a palindrome.' )
+    else:
+        print( '-> This is not a palindrome.' )
 
 if __name__ == '__main__':
     main()
